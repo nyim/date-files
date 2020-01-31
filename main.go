4b50778a9872d16fb4a940e7c5976bac61de3b8b
Nicholas Yim <nickyim05@gmail.com>
2020-01-31 15:51:34 +0800

    temp save

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "crypto/sha1"
     "fmt"
     "io"
+    "io/ioutil"
     "log"
     "os"
     "path/filepath"
@@ -23,6 +24,14 @@ type file_info struct {
     ext string
 }
 
+func (f* file_info) DstDir(setting env_setting) string {
+    return setting.root + "/" + f.month
+}
+
+func (f* file_info) DstPath(setting env_setting) string {
+    return f.DstDir(setting) + "/" + f.date + "-" + f.hash + f.ext
+}
+
 func (f* file_info) Hash() string {
     fd, err := os.Open(f.path)
     if err != nil {
@@ -37,6 +46,73 @@ func (f* file_info) Hash() string {
     }
     f.hash = fmt.Sprintf("%x", h.Sum(nil))
     return f.hash
+}
+
+func (f* file_info) MoveFile(setting env_setting, done_files map[string]*file_info) {
+
+    done, found:= done_files[f.hash]
+
+    if found {
+        if f.os_info.ModTime().Before(done.os_info.ModTime()) {
+            log.Println("removing done file:", done.path, "newer than todo file:", f.path)
+            if err := os.Remove(f.path); err != nil {
+                log.Fatal("failed, ", err)
+            }
+            //todo file to be moved
+
+        } else {
+            log.Println("removing todo file:", f.path, "newer than done file:", done.path)
+            if err := os.Remove(f.path); err != nil {
+                log.Fatal("failed, ", err)
+            }
+            //file in place
+            return
+        }
+    }
+
+    dir := f.DstDir(setting)
+    dst := f.DstPath(setting)
+
+    if dst == f.path {
+        log.Println("skip:", f.path)
+        done_files[f.hash] = f
+        return
+    }
+
+    _, err := os.Lstat(dst)
+    //do not overwrite file, move to tmp name
+    if err == nil {
+        tmpfile, err := ioutil.TempFile(setting.root, "*"+f.ext)
+        if err != nil {
+            log.Fatal("fail creating temp file, error", err)
+            return
+        }
+        dst = tmpfile.Name()
+        tmpfile.Close()
+        log.Println("moving", f.path, "to", dst)
+        err = os.Rename(f.path, dst)
+        if err != nil {
+            log.Fatal("failed", err)
+            return
+        }
+        return
+    }
+
+    err = os.MkdirAll(dir, 0755)
+    if err != nil {
+        log.Fatal("fail create dir: ", dir, "error:", err)
+        return
+    }
+
+    log.Println("moving", f.path, "to", dst)
+    err = os.Rename(f.path, dst)
+    if err != nil {
+        log.Fatal("failed", err)
+        return
+    }
+    f.path = dst
+    done_files[f.hash] = f
+
 }
 
 func NewFileInfo(info os.FileInfo, path string) *file_info {
@@ -87,50 +163,6 @@ func FindTodoFiles(setting env_setting) ([]file_info, error) {
     return list, nil
 }
 
-func MoveFile(setting env_setting, n *file_info, done_files map[string]*file_info) {
-
-    old, found:= done_files[n.hash]
-    if found {
-        if n.os_info.ModTime().After(old.os_info.ModTime()) || (n.month == old.month && n.date == old.date){
-            log.Println("removing newer file:", n.path)
-            if err := os.Remove(n.path); err != nil {
-                log.Fatal("failed, ", err)
-            }
-            return
-        }
-    }
-
-    dir := setting.root + "/" + n.month
-    dst := dir + "/" + n.date + "-" + n.hash + n.ext
-
-    if dst == n.path {
-        log.Println("skip:", n.path)
-        done_files[n.hash] = n
-        return
-    }
-
-    err := os.MkdirAll(dir, 0755)
-    if err != nil {
-        log.Fatal("fail create dir: ", dir, "error:", err)
-        return
-    }
-
-    log.Println("moving %s to %s", n.path, dst)
-    err = os.Rename(n.path, dst)
-    if err != nil {
-        log.Fatal("failed", err)
-        return
-    }
-    n.path = dst
-    done_files[n.hash] = n
-
-    if found {
-        log.Println("removing exist file:", old.path)
-        if err := os.Remove(old.path); err != nil {
-            log.Fatal("failed, ", err)
-        }
-    }
-}
 
 func main() {
 
@@ -150,8 +182,7 @@ func main() {
     done_files := make(map[string]*file_info)
 
     for i := 0; i < len(todo_files); i++ {
-        f := &todo_files[i]
-        MoveFile(setting, f, done_files)
+        todo_files[i].MoveFile(setting, done_files)
     }
 
 }
